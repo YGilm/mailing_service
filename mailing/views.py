43901a4b5aba2b4b79fc968ab5fc1df5b0660069
ygilm <ygilm@bk.ru>
2023-10-05 21:10:01 +0400

    Changed

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
 
-# Create your views here.
+from mailing.forms import MailingForm
+from .models import MailingSettings, Client, MailingMessage, MailingLog
+from .services import send_mailing
+
+
+class MailingListView(ListView):
+    model = MailingSettings
+    template_name = 'mailing_list.html'
+
+    def get_queryset(self):
+        return MailingSettings.objects.all()
+
+
+class MailingCreateView(CreateView):
+    model = MailingSettings
+    form_class = MailingForm
+    template_name = 'mailing_form.html'
+    success_url = reverse_lazy('mailing_list')
+
+    def form_valid(self, form):
+        mailing = form.save()
+        if mailing.time <= timezone.now():
+            send_mailing()
+
+        return super().form_valid(form)
+
+
+class MailingDetailView(DetailView):
+    model = MailingSettings
+    template_name = 'mailing_detail.html'
+
+
+class MailingDeleteView(DeleteView):
+    model = MailingSettings
+    template_name = 'mailing_confirm_delete.html'
+    success_url = reverse_lazy('mailing_list')
+
+
+class MailingUpdateView(UpdateView):
+    model = MailingSettings
+    form_class = MailingForm
+    template_name = 'mailing_form.html'
+    success_url = reverse_lazy('mailing_list')
