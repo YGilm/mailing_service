f14e990b51b979d8d2dfbe74a8f9630fcddb1298
ygilm <ygilm@bk.ru>
2023-10-09 16:36:43 +0400

    Added file for functions

--- mailing/services.py
+++ mailing/services.py
@@ -1,29 +1,40 @@
 from django.core.mail import send_mail
 from django.utils import timezone
 import os
+
+from config import settings
 from mailing.models import MailingSettings, Client, MailingMessage, MailingLog
 
 
 def send_mailing():
-    mailing = MailingSettings.objects.filter(time__lte=timezone.now(),
-                                             status="R").first()
-    if mailing:
-        clients = Client.objects.all()
+    print("Отправляется рассылка")
+    mailings = MailingSettings.objects.filter(time__lte=timezone.now(), status="R")
+    print(f"Найдена {mailings.count()} рассылка для отправки.")
+    for mailing in mailings:
+        print(f"Обработка рассылки: {mailing}")
+        clients = mailing.recipients.all().distinct()
         mailing_message = MailingMessage.objects.filter(mailing_settings=mailing).first()
+
+        if not mailing_message:
+            print(f"Не найдено сообщение для отправки: {mailing}")
+            continue
+
+        recipients_emails = [client.email for client in clients]
+        print(f"Отправка рассылки на почту: {len(recipients_emails)} получателя.")
 
         for client in clients:
             try:
                 send_mail(
                     mailing_message.subject,
                     mailing_message.body,
-                    os.getenv('EMAIL_HOST_USER'),  # Отправитель
+                    os.getenv(settings.EMAIL_HOST_USER),  # Отправитель
                     [client.email],  # Получатель
                 )
-                log_status = True
-                server_response = "Successfully sent"
             except Exception as e:
                 log_status = False
                 server_response = str(e)
+                # Здесь вы можете добавить дополнительное логирование ошибок
+                print(f"Ошибка отправки рассылки на {client.email}: {e}")
 
             # Логирование отправки
             MailingLog.objects.create(
@@ -32,7 +43,5 @@ def send_mailing():
                 status=log_status,
                 server_response=server_response
             )
-
-        # Меняем статус рассылки
         mailing.status = "E"
         mailing.save()
