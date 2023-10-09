f9a4e5c925cc44e0bd42673295114ae96b4016cc
ygilm <ygilm@bk.ru>
2023-10-09 16:35:15 +0400

    Added file forms

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,8 +1,24 @@
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput
 from django import forms
-from .models import MailingSettings
+from .models import MailingSettings, Client, MailingMessage
 
 
 class MailingForm(forms.ModelForm):
     class Meta:
         model = MailingSettings
-        fields = ['time', 'periodicity', 'status']
+        fields = ['time', 'periodicity', 'status', 'recipients']
+        widgets = {
+            'time': DateTimePickerInput(format='%Y-%m-%d %H:%M:%S'),
+        }
+
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ['email', 'name', 'comment']
+
+
+class MailingMessageForm(forms.ModelForm):
+    class Meta:
+        model = MailingMessage
+        fields = ['subject', 'body']
