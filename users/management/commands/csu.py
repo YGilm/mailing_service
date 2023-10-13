b5d58936b4c39e26ccfb97c5901ae8114807bbf2
ygilm <ygilm@bk.ru>
2023-10-13 05:23:07 +0400

    Added new file for superuser create

---
+++ users/management/commands/csu.py
@@ -0,0 +1,16 @@
+import os
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email=os.getenv('SUPERUSER_EMAIL'),
+            is_superuser=True,
+            is_staff=True,
+            is_active=True,
+        )
+        user.set_password(os.getenv('SUPERUSER_PASSWORD'))
+        user.save()
